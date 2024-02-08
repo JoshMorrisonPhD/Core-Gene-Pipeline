e2d65f6e8a6993a9bcd1953b16ead541e3a2b0ca
JoshMorrisonPhD <joshua@joshmorrisons.com>
2024-02-08 23:30:25 +0000

    made code better
    changed the algorithm for the comparison, solved the duplicates and the non-included peppan locus', improved regex matching functionality

--- generate_unique_core_gene_tags.py
+++ generate_unique_core_gene_tags.py
@@ -3,31 +3,23 @@ import os
 from os import path
 import sys
 import fnmatch
+import collections
 
 # Function to check if the line is for reference_species and extract old locus tag
-def extract_old_locus_tag_for_species(line, reference_strain_id):
-    if not reference_strain_id:
-        sys.exit("Must supply reference_strain_id")
-
-    if line.startswith(reference_strain_id):
-        #print(f'extract_old_locus_tag_for_species-> found line starting with reference_strain_id: {reference_strain_id}')
-        old_tag_match = re.search(r"old_locus_tag=([^;\n:]+)", line)
-        #if old_tag_match and old_tag_match.group(1):
-        #    print(f'old_tag_match: {old_tag_match.group(1)}')
-        #else:
-        #    print("old_tag_match: None")
-        return old_tag_match.group(1) if old_tag_match else None
-    return None
+def extract_old_locus_tag_for_species(line):
+    old_tag_match = re.search(r"old_locus_tag=([^;\n:]+)", line)
+    return old_tag_match.group(1) if old_tag_match else None
 
 species_folder_names = ["Agalactiae", "Equi", "Pneumo", "Suis", "Uberis"] # Add Iniae later, TODO: process "All" later
+#species_folder_names = ["Equi"] # Add Iniae later, TODO: process "All" later
 species_to_reference_strain_id = { "Agalactiae": "Agal_01173",
                     "Equi": "Equi_4047", 
                     "Pneumo": "Pneumo_TIGR4", 
                     "Suis": "Suis_P17", 
                     "Uberis": "Uberis_0140J"
+                    #"All": "Uberis_0140"
                     }
-                    #"Iniae: "STUFF",
-                    #"All": "Agal_01173" }
+                    #"Iniae: "STUFF" }
 
 for species in species_folder_names:
     print(f'Processing species {species} and mapping peppan and reference locus tags...')
@@ -44,15 +36,15 @@ for species in species_folder_names:
         sys.exit(f'Can not find the peppan output folder at {peppan_folder} for the species {species}')
     
     core_peppan_gene_locuses_file_name = None
-    for file in os.listdir(peppan_folder):
+    for file in os.listdir(species_folder):
         if fnmatch.fnmatch(file, '*_core_peppan_gene_locuses.txt'):
             core_peppan_gene_locuses_file_name = file
             break
     if not core_peppan_gene_locuses_file_name:
         sys.exit("Did you forget to run the R code to generate the output?")
-    p#rint(f'core_peppan_gene_locuses_file_name: {core_peppan_gene_locuses_file_name}')
+    #print(f'core_peppan_gene_locuses_file_name: {core_peppan_gene_locuses_file_name}')
 
-    core_peppan_gene_locuses_file_path = path.join(peppan_folder, core_peppan_gene_locuses_file_name)
+    core_peppan_gene_locuses_file_path = path.join(species_folder, core_peppan_gene_locuses_file_name)
     #print(f'core_peppan_gene_locuses_file_path: {core_peppan_gene_locuses_file_path}')
 
     # Path to the .gff file
@@ -62,6 +54,7 @@ for species in species_folder_names:
     # Paths for the output .txt files
     output_reference_tags_file_path = path.join(species_folder, 'core_' + species + '_locus_tags.txt')
     output_peppan_tags_not_found_file_path = path.join(species_folder, "peppan_locus_tags_not_found.txt")
+    output_duplicate_tags_file_path = path.join(species_folder, "duplicate_tags_found.txt")
 
     #print(f'output_reference_tags_file_path: {output_reference_tags_file_path}')
     #print(f'output_peppan_tags_not_found_file_path: {output_peppan_tags_not_found_file_path}')
@@ -70,36 +63,71 @@ for species in species_folder_names:
     species_specific_mapping = {}
     with open(peppan_gff_file_path, 'r') as gff_file:
         for line in gff_file:
-            old_tag = extract_old_locus_tag_for_species(line, species_to_reference_strain_id[species])
-            new_tag_match = re.search(r"ortholog_group:[^:]+:([^:;\n]+)", line)  # Match the new locus tag
-            if old_tag and new_tag_match:
-                new_tag = new_tag_match.group(1)
-                print(f'found new tag {new_tag} -> points to old tag {old_tag}')
-                species_specific_mapping[new_tag] = old_tag
+            # Only pick lines starting with reference_strain_id (e.g Equi_4047)
+            if line.startswith(species_to_reference_strain_id[species]):
+                # Does the line have a peppan locustag?
+                #new_tag_match = re.search(r"ortholog_group:[^:]+:([^:;\n]+)", line)  # Match the new locus tag
+
+                ortholog_group_content = re.search(r"ortholog_group:([^;]+)", line)
+                if ortholog_group_content:
+                    # Step 2: Split the content by ',GCF_' to handle multiple entries
+                    # Prepend 'GCF_' to each split part except the first one to restore the cut-off part
+                    entries = ['GCF_' + entry if i != 0 else entry for i, entry in enumerate(ortholog_group_content.group(1).split(',GCF_'))]
+
+                    # Step 3: Extract the "RS tag" from each segment
+                    new_tag_matches = [re.search(r":([^:]+):", entry).group(1) for entry in entries if re.search(r":([^:]+):", entry)]
+
+                    # Create an entry in the species specific lookup table if we have a peppan locus tag (old_locus_tag can either exist or not)
+                    for new_tag_match in new_tag_matches:
+                        old_tag = extract_old_locus_tag_for_species(line)
+                        new_tag = new_tag_match
+                        species_specific_mapping[new_tag] = old_tag
+
     print(f'Entries in species_specific_mapping (Full data dictionary): {len(species_specific_mapping)}')
 
     # Map the core genes (new locus tags) to old locus tags for the specified species
     with open(core_peppan_gene_locuses_file_path, 'r') as core_peppan_gene_locuses_file:
-        file_content = [line.strip('"\n') for line in core_peppan_gene_locuses_file.readlines()]  # Strip quotes and newline characters
-        core_genes_species_old_tags = [species_specific_mapping.get(gene) for gene in file_content if gene in species_specific_mapping]
-        print(f'Entries in core_genes_species_old_tags: {len(core_genes_species_old_tags)}')
+         # Strip quotes and newline characters
+        file_content = [line.strip('"\n') for line in core_peppan_gene_locuses_file.readlines()]
 
-    # Filter out None values and create a unique set of old locus tags
-    unique_core_genes_old_tags = set(tag for tag in core_genes_species_old_tags if tag)
-    print(f'Entries in unique_core_genes_old_tags: {len(unique_core_genes_old_tags)}')
+        # List of dictionary values (old_locus_tag) for peppan locus tag keys found in dictionary
+        core_genes_species_old_tags = [species_specific_mapping.get(locus_tag) for locus_tag in file_content if locus_tag in species_specific_mapping]
 
+        # List of dictionary values (old_locus_tag) without None values
+        core_genes_old_tags_values = [tag for tag in core_genes_species_old_tags if tag]
+
+        duplicate_core_genes_old_tags_values = [duplicate for duplicate, count in collections.Counter(core_genes_old_tags_values).items() if count > 1]
+
+        duplicate_core_genes_old_tags = {}
+        for duplicate_locus_tag in duplicate_core_genes_old_tags_values:
+            keys = [key for key, v in species_specific_mapping.items() if v == duplicate_locus_tag]
+            duplicate_core_genes_old_tags[duplicate_locus_tag] = keys
+            
+        unique_core_genes_old_tags = set(core_genes_old_tags_values)
+
+        # List of peppan locus tag keys for which dictionary has no value for that key
+        new_tags_not_found = [gene for gene in file_content if species_specific_mapping.get(gene) is None]
+            
     # Count and a sample of unique old locus tags
-    #print(f'Sample output length: {len(unique_core_genes_old_tags)}, sample: {list(unique_core_genes_old_tags)[:10]}')
-
+    print(f'Number of lines in core_peppan_gene_locuses_txt: {len(file_content)}')
+    print(f'Entries in core_genes_species_old_tags: {len(core_genes_species_old_tags)}')
+    print(f'Number of duplicate old_locus_tags: {len(duplicate_core_genes_old_tags_values)}')
+    print(f'Entries in new_tags_not_found: {len(new_tags_not_found)}')
+    print(f'Entries in unique_core_genes_old_tags: {len(unique_core_genes_old_tags)}')
+    
     # Writing the old locus tags that are present to a file
     with open(output_reference_tags_file_path, 'w') as file:
         for tag in unique_core_genes_old_tags:
             file.write(f'{tag}\n')
 
-    # Finding the new locus tags that couldn't be found and writing them to a file
-    with open(core_peppan_gene_locuses_file_path, 'r') as core_peppan_gene_locuses_file:
-        file_content = [line.strip('"\n') for line in core_peppan_gene_locuses_file.readlines()]  # Strip quotes and newline characters
-        new_tags_not_found = [gene for gene in core_peppan_gene_locuses_file.readlines() if gene not in species_specific_mapping]
-        with open(output_peppan_tags_not_found_file_path, 'w') as file:
-            for tag in new_tags_not_found:
-                file.write(f'{tag}\n')
+    with open(output_peppan_tags_not_found_file_path, 'w') as file:
+        for tag in new_tags_not_found:
+            file.write(f'{tag}\n')
+
+    with open(output_duplicate_tags_file_path, 'w') as file:
+        for seq_value, duplicate_keys in duplicate_core_genes_old_tags.items():
+            file.write(f'{seq_value}\t')
+            for duplicate_key in duplicate_keys:
+                file.write(f'{duplicate_key}\t')
+            file.write("\n")
+    print("\n")
