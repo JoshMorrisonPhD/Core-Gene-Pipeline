5211da67b4ba2e1562debf82d434e8d68078e009
JoshMorrisonPhD <joshua@joshmorrisons.com>
2024-02-20 04:31:50 +0000

    updated script to use species prefix instead of generate a folder

--- essential_gene_extractor.py
+++ essential_gene_extractor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 import os
 
-def filter_locus_tags(input_file, output_dir):
+def filter_locus_tags(input_file, species_prefix):
     # Load the spreadsheet
     df = pd.read_excel(input_file)
 
@@ -12,13 +12,9 @@ def filter_locus_tags(input_file, output_dir):
     # Filter for locus tags with 0 test_num_insertions_mapped_per_feat but type is not CDS
     non_cds_essential_locus_tags = df[(df['test_num_insertions_mapped_per_feat'] == 0) & (df['type'] != 'CDS')]['locus_tag']
 
-    # Ensure the output directory exists
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
     # Save the lists to separate .txt files
-    cds_file_path = os.path.join(output_dir, 'cds_essential_locus_tags.txt')
-    non_cds_file_path = os.path.join(output_dir, 'non_cds_essential_locus_tags.txt')
+    cds_file_path = species_prefix + '_essential_locus_tags.txt'
+    non_cds_file_path = species_prefix + '_non_cds_essential_locus_tags.txt'
 
     # Write CDS essential locus tags to file and print count
     with open(cds_file_path, 'w') as file:
@@ -40,7 +36,7 @@ def filter_locus_tags(input_file, output_dir):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Filter locus tags based on criteria.")
     parser.add_argument("-i", "--input", required=True, help="Input Excel file path.")
-    parser.add_argument("-o", "--output", required=True, help="Output directory for the text files.")
+    parser.add_argument("-o", "--output", required=True, help="Output species specific CDS essential locus tags as a text file.")
     
     args = parser.parse_args()
     
